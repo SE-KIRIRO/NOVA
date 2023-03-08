e0990c0cd92a893acf3001f56cbe98ea81d87e36
SE-KIRIRO <lawrencekiriro@gmail.com>
2023-03-08 21:01:03 +0300

    pagination done and dusted

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template, session, abort, redirect, url_for, flash, current_app
+from flask import render_template, session, abort, request, redirect, url_for, flash, current_app
 from datetime import datetime
 from .forms import NameForm, PostForm, EditProfileForm, EditProfileAdminForm
 from ..models import User, Role, Permission, Post
@@ -19,8 +19,11 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for("main.index"))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html" ,form=form, posts=posts)
+    page = request.args.get("page", 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=current_app.config["NOVA_POSTS_PER_PAGE"], error_out=False)
+    posts = pagination.items
+    return render_template("index.html" ,form=form, posts=posts, pagination=pagination)
 
 
 @main.route("/user<username>")
@@ -28,8 +31,11 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template("user.html", user=user, posts=posts)
+    page= request.args.get("page", 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=current_app.config["NOVA_POSTS_PER_PAGE"], error_out=False)
+    posts = pagination.items
+    return render_template("user.html", user=user, posts=posts, pagination=pagination)
 
 @main.route("/edit-profile", methods=["GET", "POST"])
 @login_required
